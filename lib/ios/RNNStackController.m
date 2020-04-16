ef100cab3d336488b227931f1c94487e2b604556
Les Pozdena <les@Less-MacBook-Pro.local>
2020-04-16 11:19:23 -0700

    Manual merge for renamed class: RNNNavigationController.m became RNNStrackController.m (at least, I think it did!)

--- lib/ios/RNNStackController.m
+++ lib/ios/RNNStackController.m
@@ -2,6 +2,20 @@
 #import "RNNComponentViewController.h"
 #import "UIViewController+Utils.h"
 #import "StackControllerDelegate.h"
+#import "SSWAnimator.h"
+#import "SSWDirectionalPanGestureRecognizer.h"
+
+@interface RNNStackController() <UIGestureRecognizerDelegate>
+
+@property (nonatomic, strong) NSMutableDictionary* originalTopBarImages;
+
+@property (weak, readwrite, nonatomic) UIPanGestureRecognizer *panRecognizer;
+@property (strong, nonatomic) SSWAnimator *sswAnimator;
+@property (strong, nonatomic) UIPercentDrivenInteractiveTransition *interactionController;
+/// A Boolean value that indicates whether the navigation controller is currently animating a push/pop operation.
+@property (nonatomic) BOOL duringAnimation;
+
+@end
 
 @implementation RNNStackController {
     UIViewController* _presentedViewController;
@@ -26,6 +40,19 @@
 - (void)viewDidLayoutSubviews {
 	[super viewDidLayoutSubviews];
 	[self.presenter applyOptionsOnViewDidLayoutSubviews:self.resolveOptions];
+
+	self.sswAnimator = [[SSWAnimator alloc] init];
+
+	SSWDirectionalPanGestureRecognizer *panRecognizer = [[SSWDirectionalPanGestureRecognizer alloc] initWithTarget:self action:@selector(pan:)];
+	panRecognizer.direction = SSWPanDirectionRight;
+	panRecognizer.maximumNumberOfTouches = 1;
+	panRecognizer.delegate = self;
+
+	self.panRecognizer = panRecognizer;
+
+	[self.view addGestureRecognizer:self.panRecognizer];
+
+	self.delegate = self;
 }
 
 - (void)mergeChildOptions:(RNNNavigationOptions *)options child:(UIViewController *)child {
@@ -70,6 +97,89 @@
 
 - (UIInterfaceOrientationMask)supportedInterfaceOrientations {
     return [self.presenter getOrientation];
+}
+
+#pragma mark - UIPanGestureRecognizer
+
+- (void)pan:(UIPanGestureRecognizer*)recognizer
+{
+	UIView *view = self.view;
+	if (recognizer.state == UIGestureRecognizerStateBegan) {
+		if (self.viewControllers.count > 1 && !self.duringAnimation) {
+			self.delegate = self;
+			self.interactionController = [[UIPercentDrivenInteractiveTransition alloc] init];
+			self.interactionController.completionCurve = UIViewAnimationCurveEaseOut;
+			
+			[self popViewControllerAnimated:YES];
+		}
+	} else if (recognizer.state == UIGestureRecognizerStateChanged) {
+		self.topViewController.view.userInteractionEnabled = NO;
+		CGPoint translation = [recognizer translationInView:view];
+		// Cumulative translation.x can be less than zero because user can pan slightly to the right and then back to the left.
+		CGFloat d = translation.x > 0 ? translation.x / CGRectGetWidth(view.bounds) : 0;
+		[self.interactionController updateInteractiveTransition:d];
+	} else if (recognizer.state == UIGestureRecognizerStateEnded || recognizer.state == UIGestureRecognizerStateCancelled) {
+		if ([recognizer velocityInView:view].x > 0) {
+			[self.interactionController finishInteractiveTransition];
+		} else {
+			[self.interactionController cancelInteractiveTransition];
+			// When the transition is cancelled, `navigationController:didShowViewController:animated:` isn't called, so we have to maintain `duringAnimation`'s state here too.
+			self.duringAnimation = NO;
+		}
+		self.interactionController = nil;
+		self.topViewController.view.userInteractionEnabled = YES;
+	}
+}
+
+#pragma mark - UINavigationControllerDelegate
+
+- (id<UIViewControllerAnimatedTransitioning>)navigationController:(UINavigationController *)navigationController animationControllerForOperation:(UINavigationControllerOperation)operation fromViewController:(UIViewController *)fromVC toViewController:(UIViewController *)toVC
+{
+	if (operation == UINavigationControllerOperationPop) {
+		return self.sswAnimator;
+	}
+	return nil;
+}
+
+- (id<UIViewControllerInteractiveTransitioning>)navigationController:(UINavigationController *)navigationController interactionControllerForAnimationController:(id<UIViewControllerAnimatedTransitioning>)animationController
+{
+	return self.interactionController;
+}
+
+- (void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+	if (animated) {
+		self.duringAnimation = YES;
+	}
+}
+
+- (void)navigationController:(UINavigationController *)navigationController didShowViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+	self.duringAnimation = NO;
+	
+	if (navigationController.viewControllers.count <= 1) {
+		self.panRecognizer.enabled = NO;
+	}
+	else {
+		self.panRecognizer.enabled = YES;
+	}
+}
+
+#pragma mark - UIGestureRecognizerDelegate
+
+-(BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch{
+	return UIMenuController.sharedMenuController.isMenuVisible == NO;
+}
+
+-(BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer {
+	return self.navigationController.viewControllers.count > 1;
+}
+
+-(BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(nonnull UIGestureRecognizer *)otherGestureRecognizer {
+	if ([otherGestureRecognizer isKindOfClass:NSClassFromString(@"RCTTouchHandler")] && [otherGestureRecognizer respondsToSelector:@selector(cancel)]) {
+		[(id)otherGestureRecognizer cancel];
+	}
+	return NO;
 }
 
 @end
